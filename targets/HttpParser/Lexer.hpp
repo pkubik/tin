2c7bd488f45119fb1df6e14bebe7e8d29a0a7f43
Pawel Kubik <p.kubik.pl@gmail.com>
2015-12-07 21:34:19 +0100

    parser: Move function definitions to the cpp file and add namespace http.

--- targets/HttpParser/Lexer.hpp
+++ targets/HttpParser/Lexer.hpp
@@ -32,56 +32,13 @@ struct Token {
 class Lexer {
 public:
     Lexer(BufferedInput& source) : source(source) {}
-
-    Token getToken() {
-        if (!source) {
-            return Token{Token::Type::END, ""};
-        }
-
-        const char c = source.getChar();
-
-        if (c == ':') {
-            return Token{Token::Type::COLON, std::string(1, c)};
-        }
-
-        if (::isspace(static_cast<int>(c))) {
-            if (::isblank(static_cast<int>(c))) {
-                return Token{Token::Type::BLANK, std::string(1, c)};
-            } else {
-                auto token = Token{Token::Type::CRLF, std::string(1, c)};
-
-                if (c == '\r') {
-                    finishCrlf(token);
-                }
-
-                return token;
-            }
-        }
-
-        auto token = Token{Token::Type::WORD, std::string(1, c)};
-        finishWord(token);
-        return token;
-    }
+    Token getToken();
 
 private:
     BufferedInput& source;
 
-    void finishWord(Token& token) {
-        char n = source.peekChar();
-        while (n != ':' && !::isspace(static_cast<int>(n))) {
-            token.value += n;
-            source.getChar();
-            n = source.peekChar();
-        }
-    }
-
-    void finishCrlf(Token& token) {
-        char n = source.peekChar();
-        if (n == '\n') {
-            token.value += n;
-            source.getChar();
-        }
-    }
+    void finishWord(Token& token);
+    void finishCrlf(Token& token);
 };
 
 }
