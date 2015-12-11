3b6da3e8a0a0a316806c2ebacc435ddd164fb425
Pawel Kubik <p.kubik.pl@gmail.com>
2015-12-11 22:30:58 +0100

    Request class

--- targets/HttpParser/HttpParser.hpp
+++ targets/HttpParser/HttpParser.hpp
@@ -9,8 +9,40 @@
 
 #pragma once
 
+#include <string>
+#include <unordered_map>
+
 namespace parser {
+namespace http {
 
-int increment(int& x);
+class Request {
+public:
+    enum Method {
+        GET,
+        POST,
+        HEAD
+    };
 
+    typedef std::unordered_map<std::string, std::string> HeaderMap;
+
+    Request() = default;
+    Request(const Request&) = delete;
+    Request& operator =(const Request&) = delete;
+    Request(Request&&) = default;
+
+    Method getMethod() const { return method; }
+    const std::string& getUri() const { return uri; }
+    const std::string& getVersion() const { return version; }
+    const HeaderMap& getHeaders() const { return headers; }
+
+private:
+    Method method;
+    std::string uri;
+    std::string version;
+
+    HeaderMap headers;
+    // message body is left in the buffer for another parser
+};
+
+}
 }
